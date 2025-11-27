489e149202fa9b8b506806659c3270b66664c46c
Jose <jandresmongerayo@gmail.com>
2025-11-27 05:29:40 -0600

    Se agregan las fotos de galeria y servicios
    agregue las fotos de la galeria y los servicios para que no se vea tan feo

--- servicios.php
+++ servicios.php
@@ -18,7 +18,7 @@
 
         <!-- Servicio: Telefonía Móvil -->
         <div class="card col-sm-12 col-md-6 col-lg-3 me-3 mt-3 p-4">
-            <img src="img/telefonia-movil.jpg" class="card-img-top" alt="telefonía móvil ICE">
+            <img src="img/telefoniaMovil.jpg" class="card-img-top" alt="telefonía móvil ICE">
             <div class="card-body text-center">
                 <h5 class="card-title">Telefonía Móvil</h5>
                 <p class="card-text">Planes y servicios móviles bajo la marca kölbi, con cobertura nacional.</p>
@@ -28,7 +28,7 @@
 
         <!-- Servicio: Internet -->
         <div class="card col-sm-12 col-md-6 col-lg-3 me-3 mt-3 p-4">
-            <img src="img/internet.jpg" class="card-img-top" alt="internet ICE">
+            <img src="img/Internet.jpg" class="card-img-top" alt="internet ICE">
             <div class="card-body text-center">
                 <h5 class="card-title">Internet</h5>
                 <p class="card-text">Conexiones de internet fijo y móvil de alta velocidad para hogares y empresas.</p>
@@ -38,7 +38,7 @@
 
         <!-- Servicio: Telefonía Fija -->
         <div class="card col-sm-12 col-md-6 col-lg-3 me-3 mt-3 p-4">
-            <img src="img/telefonia-fija.jpg" class="card-img-top" alt="telefonía fija ICE">
+            <img src="img/telefoniaFija.jpg" class="card-img-top" alt="telefonía fija ICE">
             <div class="card-body text-center">
                 <h5 class="card-title">Telefonía Fija</h5>
                 <p class="card-text">Servicio de telefonía tradicional para hogares y negocios, con amplia cobertura nacional.</p>
@@ -48,7 +48,7 @@
 
         <!-- Servicio: Televisión por Suscripción -->
         <div class="card col-sm-12 col-md-6 col-lg-3 me-3 mt-3 p-4">
-            <img src="img/television.jpg" class="card-img-top" alt="televisión ICE">
+            <img src="img/televisionSuscripcion.jpg" class="card-img-top" alt="televisión ICE">
             <div class="card-body text-center">
                 <h5 class="card-title">Televisión por Suscripción</h5>
                 <p class="card-text">Paquetes de televisión digital con variedad de canales nacionales e internacionales.</p>
@@ -58,7 +58,7 @@
 
         <!-- Servicio: Soluciones Empresariales -->
         <div class="card col-sm-12 col-md-6 col-lg-3 me-3 mt-3 p-4">
-            <img src="img/soluciones-empresariales.jpg" class="card-img-top" alt="soluciones empresariales ICE">
+            <img src="img/solucionesEmpresariales.jpg" class="card-img-top" alt="soluciones empresariales ICE">
             <div class="card-body text-center">
                 <h5 class="card-title">Soluciones Empresariales</h5>
                 <p class="card-text">Servicios de comunicación y energía diseñados para apoyar el crecimiento de empresas.</p>
