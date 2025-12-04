0facc37bb2a92c46360580ca7fbaff44166b08b9
LUIS C. ZAMORA G. <luis.zamora012@gmail.com>
2025-12-03 18:38:33 -0600

    Para solucionar error de envio de correo

--- shared/footer.php
+++ shared/footer.php
@@ -31,6 +31,9 @@
 <script src="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
+<?php
+$nombreArchivo = $nombreArchivo ?? basename($_SERVER['PHP_SELF'], '.php');
+?>
 
 <?php
 switch ($nombreArchivo) {
